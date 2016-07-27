88f744545b20e9f1aaa16c469e546e213e2fee78
AfroMonkey <moisalejandro@gmail.com>
2016-07-27 12:04:09 -0500

    [REM] expiration_date

--- models/waterhole.py
+++ models/waterhole.py
@@ -7,7 +7,6 @@ class Waterhole(models.Model):
     name = fields.Char(string="Watterhole ID", required=True)
     owner = fields.Many2one('res.partner', required=True)
     cubic_meters = fields.Float(required=True)
-    expiration_date = fields.Date(required=True)
     coordinates = fields.Char(required=True)
 
     _sql_constraints = [
