38180ff49c8967d84bb9a40a8bbbb93846afd67f
AfroMonkey <moisalejandro@gmail.com>
2016-07-27 11:43:21 -0500

    [IMP] Model fields

--- models/waterhole.py
+++ models/waterhole.py
@@ -3,3 +3,9 @@ from openerp import api, fields, models
 
 class Waterhole(models.Model):
     _name = 'waterhole'
+
+    name = fields.Char(string="Watterhole ID", required=True)
+    Owner = fields.Many2one('res.partner', required=True)
+    Cubic meters = fields.Float(required=True)
+    Expiration date = fields.Date(required=True)
+    Coordinates = fields.Char(required=True)
