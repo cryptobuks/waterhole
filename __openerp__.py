5b7f10bc41bcded2d408de09782f12670e39ead2
AfroMonkey <moisalejandro@gmail.com>
2016-10-03 17:31:38 -0500

    [FIX]

--- __openerp__.py
+++ __openerp__.py
@@ -5,8 +5,8 @@
     'website': 'http://humanytek.com',
     'depends': ['purchase', 'purchase_contract_type'],
     'data': [
-        'security/ir.model.access.csv',
         'security/waterhole_access_rules.xml',
+        'security/ir.model.access.csv',
         'views/waterhole.xml',
         "views/purchase_waterhole.xml",
     ]
